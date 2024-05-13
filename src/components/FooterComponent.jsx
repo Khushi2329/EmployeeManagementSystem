dc5105614c6e049aabc8288b7dc5f2fc340d257e
Khushi2329 <103629987+Khushi2329@users.noreply.github.com>
2024-05-13 18:24:23 +0530

    Add files via upload

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+<span>
+    abcdef for reservation
+</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
